1d3e26a9e0d3a8f55383b53b2ced0cbc96c947d4
20201003683_石绣文 <2729676142@qq.com>
2023-05-08 18:14:46 +0800

    双人对战

---
+++ serve/play_menu_human4.py
@@ -0,0 +1,323 @@
+import tkinter as tk
+from tkinter import Button, messagebox
+from tkinter.messagebox import showinfo, showwarning, showerror
+import time
+import socket
+import sys
+#from play_menu_human import *
+
+HOST = '127.0.0.1'
+# HOST = '192.168.43.238'  # 服务端地址
+PORT = 5000        # 服务端监听的端口号
+
+
+flag_color='0'
+num = b'12'
+# 打包报头部分
+def packet_head(port, num, type_message, fin, rongyu, data):
+    my_port = port
+    server_port = port
+    serial_num = num
+    type_mes = type_message
+    FIN = fin
+    baoliu = rongyu
+    baotou = my_port + b'|' + server_port + b'|' + serial_num + b'|' + type_mes + b'|' + FIN + b'|' + baoliu
+    a_packet = baotou + b'|' + data
+    print(a_packet)
+    return a_packet
+
+# 拆包报头
+def unpacket(packet):
+    port, my_port, num, type_message, fin, rongyu, data = packet.decode().split("|")
+    print("来源端口", port, "类型", type(port))
+    print("序列号", num)
+    print("信息类型", type_message)
+    print("结束标识", fin)
+    print("数据", data)
+    return type_message , fin , data #返回值添加一个fin
+
+# 字符串转换为int，用于处理用户输入的内容
+# 传入参数：要转换的字符串
+# 返回值：转换后的结果
+def change_to_uint(str):
+    str1=str.decode()
+    # result = int.from_bytes(str, 'big')
+    result=int(str1)
+    return result
+
+
+# 转换为字节，一个数对应32位
+# 传入参数：要转换的数
+# 返回值：转换后的结果
+def change_to_bytes(my_uint):
+    str1=str(my_uint)
+    result=str1.encode()
+    return result
+
+
+def center_window(window, width, height):
+    # 获取屏幕宽度和高度
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    
+    # 计算新窗口左上角坐标
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    
+    # 设置窗口位置
+    window.geometry(f"{width}x{height}+{x}+{y}")
+
+
+def get_data():
+    count_data = data.count
+    return data,count_data
+
+   
+def play_menu_human(client_socket,flag_color):
+    # 定义时间限制
+    time_limit = 30
+
+    # 创建主窗口
+    root = tk.Toplevel()
+    root.title("五子棋")
+    center_window(root,1050,650)
+    # 加载背景图片
+    #kitty_besteng_new/yugui_new
+    img_board=tk.PhotoImage(file='E:\\网络安全\\代码\\source\\chess_board_new.png')
+    img_bchess=tk.PhotoImage(file='E:\\网络安全\\代码\\source\\heiqi_new.png')
+    img_wchess=tk.PhotoImage(file='E:\\网络安全\\代码\\source\\baiqi_new.png')
+    bg_img = tk.PhotoImage(file='E:\\网络安全\\代码\\source\\base_final3.png') #base_final3,ameng_final2,yugui_final2,kitty_final2
+    # 创建画布
+    canvas = tk.Canvas(root, width=1050, height=650)
+    canvas.pack()
+
+    # 绘制背景图片
+    canvas.create_image(0, 0, image=bg_img, anchor='nw')
+     # 创建玩家一、玩家二文本框
+    player1_name = tk.Entry(root, font=("Arial", 14), width=10, state='disabled')
+    player1_name.place(x=800, y=200)
+    player1_score = tk.Entry(root, font=("Arial", 14), width=10, state='disabled')
+    player1_score.place(x=800, y=250)
+    player2_name = tk.Entry(root, font=("Arial", 14), width=10, state='disabled')
+    player2_name.place(x=800, y=430)
+    player2_score = tk.Entry(root, font=("Arial", 14), width=10, state='disabled')
+    player2_score.place(x=800, y=480)
+
+    def surrender():
+        message_fail='认输'
+        client_socket.sendall(message_fail.encode())
+        # sys.exit()
+
+    # 创建认输按钮
+    surrender_button = tk.Button(root,text="认输", font=("华文行楷", 15), command=surrender)
+    surrender_button.place(x=900,y=590,width=80,height=49.44)
+
+   
+
+
+    wait_label=tk.Label(root, font=("华文行楷", 20),text="等待对手中........")
+    wait_label.place(relx=0.4,rely=0.4,width=200,height=50)
+
+    global data
+
+    #棋盘定义 13x13棋盘
+    chess_board=[[]]
+    def set_chess_board():
+        x,y=0,0
+        while True:
+            if x==520:
+                x=0
+                y+=40
+                if y<520:
+                    chess_board.append([])
+            if y==520:   
+                break
+            chess_board[-1].append([x,y])
+            x+=40
+    set_chess_board()
+    chess_exist=[[0 for i in range(13)]for j in range(13)]
+    #黑白棋子初始化
+    black_chess,white_chess=[],[]
+    #棋子类型
+    chess_kind=1    #1为黑棋，0为白棋
+    wcx,wcy,bcx,bcy=[],[],[],[]   #white_chess_x
+
+    def draw_board():
+        #横坐标
+        for i in chess_board:
+            for j in i:
+                # 使用Tkinter来更新界面
+                canvas.create_image(j[0]+73, j[1]+69, image=img_board)
+                canvas.update()
+    draw_board()
+    def draw_black_chess(col,row):
+        canvas.create_image(col * 40+13, row * 40+9, image=img_bchess, anchor='nw')
+        canvas.update()
+
+    def draw_white_chess(col,row):
+        canvas.create_image(col * 40+13, row * 40+9, image=img_wchess, anchor='nw')
+        canvas.update()
+
+    def draw_chess(white_chess,black_chess):
+            # 绘制白色棋子
+        for i in white_chess:
+            canvas.create_image(i[0] * 40+13, i[1] * 40+9, image=img_wchess, anchor='nw')
+        # 绘制黑色棋子
+        for i in black_chess:
+            canvas.create_image(i[0] * 40+13, i[1] * 40+9, image=img_bchess, anchor='nw')
+    
+   
+    username='2'
+    client_socket.sendall(username.encode())
+    print("我已发送自己的玩家信息给服务器!")
+    game_message=client_socket.recv(1024)
+    print("我接受到了来自服务器的分配")
+    wait_label.destroy()
+    print("game_message:",game_message)
+    if game_message.decode()=='Game Start! You are player1 (Black).':
+        flag_color='0'
+        print("我是黑棋")
+    else:
+        flag_color='1'
+        print("我是白棋")
+    
+    def game_judge(game_result):
+        if game_result=='你赢了':
+            print("你赢了")
+            wait_label=tk.Label(root, font=("华文行楷", 20),text="你赢了")
+            wait_label.place(relx=0.4,rely=0.4,width=200,height=50)
+            client_socket.close()
+            return 0
+            # sys.exit()
+        elif game_result=='你输了':
+            print("你输了")
+            wait_label=tk.Label(root, font=("华文行楷", 20),text="你输了")
+            wait_label.place(relx=0.4,rely=0.4,width=200,height=50)
+            client_socket.close()
+            return 0
+            # sys.exit()
+        else:
+            return 1
+
+    def connect_to_server(event):
+        # global flag_color
+        x, y = event.x, event.y
+        # draw_chess(white_chess,black_chess)
+        print("black_chess",black_chess)
+        print("white_chess",white_chess)
+        print(type(x))
+        col, row = (x-10) // 40, (y-10)// 40
+        if 0 <= col < 14 and 0 <= row < 14:
+            if chess_exist[col][row]==0:
+                #默认为黑棋
+                if flag_color=='0':
+                    black_chess.append([col,row])
+                    #print(col,row)
+                    print(black_chess)
+                    draw_black_chess(col,row)
+                    # draw_chess(white_chess,black_chess)
+                    bcx.append(black_chess[-1][0])
+                    bcy.append(black_chess[-1][1])
+                    my_x=str(col)
+                    print(my_x)
+                    my_y=str(row)
+                    my_color=flag_color
+                    data=my_x+','+my_y+','+my_color
+                    # message=packet_head(b'5000',num,b'2019',b'1',b'2',data)
+                    client_socket.sendall(data.encode())
+                    game_result=client_socket.recv(1024).decode()
+                    if game_judge(game_result)==1:
+                        chess_exist[col][row]=1
+                        white_message=client_socket.recv(1024).decode()
+                        if white_message=='对方认输':
+                            game_judge('你赢了')
+                        game_result2=client_socket.recv(1024).decode()
+                        #判断战局
+                        game_judge(game_result2)     
+                        col1_w, row1_w,color1=white_message.split(',')
+                        col1_w_int=int(col1_w)
+                        row1_w_int=int(row1_w)
+                        print("收到的白棋信息：",white_message)
+                        white_chess.append([col1_w_int,row1_w_int])
+                        chess_exist[col1_w_int][row1_w_int]=1
+                        draw_white_chess(col1_w_int,row1_w_int)
+                    
+                    canvas.update()
+                    #白棋
+                else:      
+                    white_chess.append([col,row])
+                    print("white_chess",white_chess)
+                    draw_white_chess(col,row)
+                    # draw_chess(white_chess,black_chess)
+                    wcx.append(white_chess[-1][0])
+                    wcy.append(white_chess[-1][1])
+                    my_wx=str(col)
+                    print(my_wx)
+                    my_wy=str(row)
+                    my_color=flag_color
+                    data2=my_wx+','+my_wy+','+my_color
+                    # message2=packet_head(b'5000',num,b'2019',b'1',b'2',data2)
+                    client_socket.sendall(data2.encode())
+                    game_result=client_socket.recv(1024).decode()
+                    if game_judge(game_result)==1:
+                    #client_socket.sendall(f'{white_chess}\n'.encode('utf-8'))
+                        chess_exist[col][row]=1
+                        black_message=client_socket.recv(1024).decode()
+                        if black_message=='对方认输':
+                            game_judge('你赢了')
+                        game_result2=client_socket.recv(1024).decode()
+                        #判断战局
+                        game_judge(game_result2)
+                        col1_b,row1_b,color1=black_message.split(',')
+                        col1_b_int=int(col1_b)
+                        row1_b_int=int(row1_b)
+                        print("收到的黑棋信息：",black_message)
+                        black_chess.append([col1_b_int,row1_b_int])
+                        draw_black_chess(col1_b_int,row1_b_int)
+                        # draw_chess(white_chess,black_chess)
+                        print("我绘制了一个黑棋")
+                        chess_exist[col1_b_int][row1_b_int]=1
+                    
+                    # draw_chess(white_chess,black_chess)
+                    # if client_socket.recv(1024).decode()=='你赢了':
+                    #     print(white_message)
+                    # elif client_socket.recv(1024).decode()=='你赢了':
+                    #     print(white_message) 
+                    canvas.update()
+        # draw_chess(white_chess,black_chess)
+        return 1
+    print("flag_coler为:",flag_color)
+    if flag_color=='1':
+        black_message=client_socket.recv(1024).decode()
+        if black_message=='对方认输':
+            game_judge('你赢了')
+        col1_b,row1_b,color1=black_message.split(',')
+        col1_b_int=int(col1_b)
+        row1_b_int=int(row1_b)
+        print("收到的黑棋信息：",black_message)
+        black_chess.append([col1_b_int,row1_b_int])
+        draw_black_chess(col1_b_int,row1_b_int)
+        # draw_chess(white_chess,black_chess)
+        print("我绘制了一个黑棋")
+        game_result1=client_socket.recv(1024).decode()
+        #判断战局
+        game_judge(game_result1)               
+        chess_exist[col1_b_int][row1_b_int]=1
+     # 绑定鼠标事件
+    canvas.bind('<Button-1>', connect_to_server)
+     
+   
+    # 运行主循环
+    root.mainloop()
+        
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((HOST, PORT))
+    
+    play_menu_human(client_socket,flag_color)   
+
+
+  
+   
+
+
+
---
+++ serve/v4.py
@@ -0,0 +1,215 @@
+import socket
+import threading
+import queue
+from collections import Counter
+HOST = '127.0.0.1'
+# HOST = '192.168.43.238'  # 服务端地址
+PORT = 5000        # 服务端监听的端口号
+
+def unpacket(packet):
+    port, my_port, num, type_message, fin, rongyu, data = packet.decode().split('|')
+    print("来源端口", port, "类型", type(port))
+    print("序列号", num)
+    print("信息类型", type_message)
+    print("结束标识", fin)
+    print("数据", data)
+    return type_message, fin, data #返回值添加一个fin
+
+def row_column_win(x,m,n,chess):
+    for i in x:
+        if x[i]>=5:
+            xy=[]
+            for j in chess:
+                if j[m]==i:
+                    xy.append(j[n])
+            xy.sort()
+            count=0
+            for j in range(len(xy)-1):
+                if xy[j]+1==xy[j+1]:
+                    count+=1
+                else:
+                    count=0
+            if count>=4:
+                return 1
+
+def xiejiao_win(chess):
+    x,y=[],[]
+    chess.sort()
+    for i in chess:
+        x.append(i[0])
+        y.append(i[1])
+    c,first,last=0,0,0
+    for i in range(len(x)-1):
+        if x[i+1]!=x[i]:
+            if x[i]+1==x[i+1]:
+                c+=1
+                last=i+1
+            else:
+                if c<4:
+                    first=i+1
+                    c=0
+                else:
+                    last=i
+                    print(last)
+                    break
+        else:
+            last=i+1
+    if c>=4:
+        dis=[]
+        for i in range(first,last+1):
+            dis.append(x[i]-y[i])
+        count=Counter(dis)
+        for i in count:
+            if count[i]>=5:
+                return 1
+        dis=[]
+        x2=[i*(-1) for i in x]
+        for i in range(first,last+1):
+            dis.append(x2[i]-y[i])
+        count=Counter(dis)
+        for i in count:
+            if count[i]>=5:
+                return 1
+
+def gameover(wcx,wcy,bcx,bcy,white_chess,black_chess):
+    wcx_count,wcy_count,bcx_count,bcy_count=Counter(wcx),Counter(wcy),Counter(bcx),Counter(bcy)
+    if row_column_win(wcx_count,0,1,white_chess)==1:
+        return 0
+    elif row_column_win(bcx_count,0,1,black_chess)==1:
+        return 1
+    elif row_column_win(wcy_count,1,0,white_chess)==1:
+        return 0
+    elif row_column_win(bcy_count,1,0,black_chess)==1:
+        return 1
+    elif xiejiao_win(white_chess)==1:
+        return 0
+    elif xiejiao_win(black_chess)==1:
+        return 1
+    else:
+        return 2
+
+def recv_zuobiao (data):
+    x,y,color = data.split(",")
+    return x,y,color
+
+def handle_client_request(client_socket,addr):
+    # 接收玩家的用户名并加入等待队列
+    print("已连接到: ",addr)
+    username = client_socket.recv(1024).decode()
+    wait_queue.put((username, client_socket))
+    print("我接受到的玩家: ",wait_queue)
+    # 如果等待队列中有两个玩家，则开始游戏
+    if wait_queue.qsize() == 2:
+        # 从等待队列中取出两个玩家并开始游戏
+        player1, socket1 = wait_queue.get()
+        player2, socket2 = wait_queue.get()
+        # 发送游戏开始的消息给两个玩家
+        socket1.sendall(('Game Start! You are player1 (Black).').encode())
+        socket2.sendall(('Game Start! You are player2 (White).').encode())
+        #黑白棋子初始化
+        black_chess,white_chess=[],[]
+        wcx,wcy,bcx,bcy=[],[],[],[] 
+        # 等待两个玩家依次下棋
+        while True:
+            # 玩家1下棋
+            position1 = socket1.recv(1024)
+            #row1, col1 = map(int, position1.split(','))
+            print(position1,type(position1))
+             #向玩家二发送黑棋信息           
+            # black_message=position1
+            if position1.decode()=='认输':
+                print("玩家一认输")
+                text="对方认输"
+                socket2.sendall(text.encode())
+                # socket2.sendall("对方认输")
+                socket1.close()
+                socket2.close()
+            else:
+                socket2.sendall(position1)
+            col1,row1,color1=position1.decode().split(",")
+            row1_int=int(row1)
+            col1_int=int(col1)
+            black_chess.append([col1_int,row1_int])
+            bcx.append(black_chess[-1][0])
+            bcy.append(black_chess[-1][1])
+            print("黑棋序列：",black_chess)
+
+            #玩家一下棋后判断战局情况
+            flag_win=gameover(wcx,wcy,bcx,bcy,white_chess,black_chess)
+            if flag_win==1:
+                print("黑棋赢了")
+                socket1.sendall(('你赢了').encode())
+                socket2.sendall(('你输了').encode())
+            elif flag_win==0:
+                print("白棋赢了")
+                socket2.sendall(('你赢了').encode())
+                socket1.sendall(('你输了').encode())
+            else:
+                print("游戏继续")
+                socket2.sendall(('游戏继续').encode())
+                socket1.sendall(('游戏继续').encode())
+
+            # 玩家2下棋
+            position2 = socket2.recv(1024)
+            #向玩家一发送白棋信息
+            # white_message=position2
+            if position2.decode()=='认输':
+                print("玩家二认输")
+                text="对方认输"
+                socket1.sendall(text.encode())
+                socket1.close()
+                socket2.close()
+            else:
+                socket1.sendall(position2)
+            # socket1.sendall(position2)
+            col2, row2,color2=position2.decode().split(',')
+            row2_int=int(row2)
+            col2_int=int(col2)
+            white_chess.append([col2_int,row2_int])
+            print("白棋序列：",white_chess)
+            wcx.append(white_chess[-1][0])
+            wcy.append(white_chess[-1][1])
+
+            #玩家二下棋后判断战局情况
+            flag_win=gameover(wcx,wcy,bcx,bcy,white_chess,black_chess)
+            if flag_win==1:
+                print("黑棋赢了")
+                socket1.sendall(('你赢了').encode())
+                socket2.sendall(('你输了').encode())
+                socket1.close()
+                socket2.close()
+            elif flag_win==0:
+                print("白棋赢了")
+                socket2.sendall(('你赢了').encode())
+                socket1.sendall(('你输了').encode())
+                socket1.close()
+                socket2.close()
+            else:
+                print("游戏继续")
+                socket2.sendall(('游戏继续').encode())
+                socket1.sendall(('游戏继续').encode())
+            
+           
+
+# 创建一个TCP/IP套接字
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 绑定服务器的地址和端口号
+server_address = (HOST, PORT)
+print('Starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
+
+# 监听连接
+sock.listen(10)#10个客户端
+print('Waiting for a connection...')
+# 创建一个等待队列，存储已经登录的玩家
+wait_queue = queue.Queue()
+
+while True:
+    # 等待连接
+    conn, addr = sock.accept()
+    print('Connection from', addr)
+
+    # 创建一个新线程处理客户端请求
+    client_thread = threading.Thread(target=handle_client_request, args=(conn, addr))
+    client_thread.start()
